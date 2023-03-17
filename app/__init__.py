b31a8514bdc7d8a9f9d7ab5164a6b199638b98b5
Martin Souto <msouto2001@gmail.com>
2023-03-17 17:33:19 -0300

    Starting the project, initial files created, database connection created, schema file completed

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from app import db
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SENDGRID_KEY=os.environ.get('SENDGRID_API_KEY'),
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+    )
+
+    db.init_app(app)
+    
+    return app
