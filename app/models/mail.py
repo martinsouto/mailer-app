7823b37bc5ed66f0c091e7baba8538ac37d2f84d
Martin Souto <msouto2001@gmail.com>
2023-05-21 14:24:56 -0300

    added create method to mail model

--- app/models/mail.py
+++ app/models/mail.py
@@ -7,3 +7,9 @@ class Mail(object):
         db, c = get_db()
         c.execute('select * from mail')
         return c.fetchall()
+    
+    @classmethod
+    def create(cls, email, subject, content):
+        db, c = get_db()
+        c.execute("insert into mail (email, subject, content) values (%s, %s, %s)", (email, subject, content))
+        db.commit()
