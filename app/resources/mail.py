a1061fe667773a0f53911679cc8898ce609d4c4e
Martin Souto <msouto2001@gmail.com>
2023-05-21 19:30:49 -0300

    added functionality to method mail.create

--- app/resources/mail.py
+++ app/resources/mail.py
@@ -10,4 +10,18 @@ def index():
 
 @bp.route('/create', methods=['POST', 'GET'])
 def create():
+    if request.method == 'POST':
+        if request.form.get('create'):
+            if not request.form['email'] or not request.form['subject'] or not request.form['content']:
+                flash('You must complete all the fields')
+            else:
+                Mail.create(request.form['email'], request.form['subject'], request.form['content'])
+                return redirect(url_for('mail.index'))
+        else:
+            return redirect(url_for('mail.index'))
+        
     return render_template('mail/create.html')
+
+def send(email, subject, content):
+    pass
+    #sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
